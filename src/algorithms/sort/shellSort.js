469188168fc9817ea36d75cf7bb3b7cf6d6d6c26
wanghauiyao <741663036@qq.com>
2019-04-18 14:24:40 +0800

    add shellSort

--- src/algorithms/sort/shellSort.js
+++ src/algorithms/sort/shellSort.js
@@ -1,4 +1,33 @@
+const producer = require("../dataproducer");
+
 //希尔排序
 function shellSort(ary) {
-    
+    var n = ary.length;
+    //计算分组增量
+    var stepInc = parseInt(n/2);
+    //
+    while(stepInc > 0) {
+        for(var i = 0; i < stepInc; i++) {
+            for( var j = i + stepInc; j < n; j += stepInc) {
+                if(ary[j] < ary[j - stepInc]) {
+                    var temp = ary[j];
+                    var k = j - stepInc;
+                    // 插入到目标位置
+                    while( k >= 0 && ary[k] > temp) {
+                        ary[k + stepInc] = ary[k];
+                        k -= stepInc;
+                    }
+                    ary[k + stepInc] = temp;
+                }
+            }
+        }
+     
+        stepInc = parseInt(stepInc / 2);
+    }
+
 }
+
+var toSort = producer.produceIntAry(12);
+console.log(toSort);
+shellSort(toSort);
+console.log(toSort);
